b31d2beb468ee72ec1d67f227221fb881f5e8546
Ryan Setzer <ryanjsetzer@gmail.com>
2024-11-17 13:35:26 -0500

    Clarified compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
-g++ backend/*.cpp -o executables/main
-diskus executables
-strip --strip-unneeded executables/main
-diskus executables
-./executables/main
+g++ backend/*.cpp -o executables/main     # Base Compile
+diskus executables                        # Print Executable Original Size
+strip --strip-unneeded executables/main   # Strip Executable of Non-Needed Libraries
+diskus executables                        # Print Executable New Size
+./executables/main                        # Run
 
